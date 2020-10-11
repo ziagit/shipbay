9d8490097e551c841ece3c2edc600ab5320617db
zia <zia.csco@gmail.com>
2020-10-11 14:26:37 +0430

    solved contactseeder not found

--- resources/js/components/frontend/shipment/ShipmentDetails.vue
+++ resources/js/components/frontend/shipment/ShipmentDetails.vue
@@ -1,95 +1,114 @@
 <template>
-  <div class="shipment-details">
-    <ion-card mode="ios" color="light">
-      <ion-card-header>
-        <ion-title>Shipment details</ion-title>
-      </ion-card-header>
-      <ion-card-content v-if="dataLoaded">
-        <ion-text>
-          <p>
-            Dear {{shipment.shipment.full_address[1].name}},
-            <br />your freight
-            <span color="danger">{{shipment.jobstatus.title}}</span>
-          </p>
-          <p>You will be collecting your freight at bellow address</p>
-          <p>{{shipment.shipment.full_address[1].city.name}}, {{shipment.shipment.full_address[1].citycode.postal_code}}, {{shipment.shipment.full_address[1].address}}</p>
+<div class="shipment-details">
+    <md-card mode="ios" color="light">
+        <md-card-header>
+            <h3 class="md-title">Shipment details</h3>
+        </md-card-header>
+        <md-card-content v-if="dataLoaded">
+            <div>
+                <p>
+                    Dear {{shipment.order_detail.shipper.first_name}},
+                    <br />your freight
+                    <span>{{shipment.jobstatus.title}}</span>
+                </p>
+                <p>You will be collecting your freight at bellow address</p>
 
-          <div>
-            <div class="carrier-contacts">
-              <ion-label>Carrier contacts</ion-label>
-              <ion-toggle @ionChange="getCarrier()"></ion-toggle>
+                <p> {{shipment.order_detail.full_address[1].country.name}},
+                    {{shipment.order_detail.full_address[1].state.name}},<br>
+                    {{shipment.order_detail.full_address[1].city.name}},
+                    {{shipment.order_detail.full_address[1].zip.postal_code}},
+                    {{shipment.order_detail.full_address[1].address.name}}
+                </p>
+
+                <div>
+                    <div class="carrier-contacts">
+                        <label>Carrier contacts</label>
+                        <md-switch v-model="carrierTogal"></md-switch>
+                    </div>
+                    <div v-show="carrierTogal">
+                        <div v-if="carrier != null">
+                            <p>{{carrier.last_name}} <br>
+                                {{carrier.contact.phone}} <br>
+                                {{carrier.contact.email}}
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div v-show="carrierTogal">
-              <ion-text v-if="carrierContacts != null">
-                <p>{{carrierContacts.last_name}}</p>
-                <p>{{carrierContacts.phone}}</p>
-              </ion-text>
-            </div>
-          </div>
-        </ion-text>
-      </ion-card-content>
-    </ion-card>
-  </div>
+        </md-card-content>
+    </md-card>
+</div>
 </template>
+
 <script>
 import axios from "axios";
 export default {
-  name: "ShipmentDetails",
-  data: () => ({
-    dataLoaded: false,
-    shipment: null,
-    carrierContacts: null,
-    carrierExist: false,
-    carrierTogal: false,
-  }),
-  methods: {
-    get() {
-      axios
-        .get("shipment-details/" + this.$route.params.id)
-        .then((res) => {
-          this.shipment = res.data;
-          this.dataLoaded = true;
-          console.log("res: ", res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+    name: "ShipmentDetails",
+    data: () => ({
+        dataLoaded: false,
+        shipment: null,
+        carrier: null,
+        carrierExist: false,
+        carrierTogal: false,
+    }),
+    watch: {
+        carrierTogal(data) {
+            if (data) {
+                this.getCarrier()
+            }
+        }
     },
-    getCarrier() {
-      this.carrierTogal = !this.carrierTogal;
-      if (this.carrierContacts == null) {
-        axios
-          .get("carrier-contacts/" + this.shipment.carrier_id)
-          .then((res) => {
-            this.carrierExist = true;
-            this.carrierContacts = res.data;
-            console.log(res.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
+    methods: {
+        get() {
+            axios
+                .get("shipment-details/" + this.$route.params.id)
+                .then((res) => {
+                    this.shipment = res.data;
+                    this.dataLoaded = true;
+                    console.log("res: ", this.shipment);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+        getCarrier() {
+            if (this.carrier == null) {
+                axios
+                    .get("carrier-contacts/" + this.shipment.carrier_id)
+                    .then((res) => {
+                        this.carrierExist = true;
+                        this.carrier = res.data;
+                        console.log(res.data);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            }
+        },
     },
-  },
-  created() {
-    this.get();
-  },
+    created() {
+        this.get();
+    },
 };
 </script>
+
 <style lang="scss" scoped>
 .shipment-details {
-  max-width: 600px;
-  margin: auto;
-  ion-card {
-    text-align: left;
-    background: #fff !important;
-    span {
-      color: green;
+    max-width: 600px;
+    margin: auto;
+
+    md-card {
+        text-align: left;
+        background: #fff !important;
+
+        span {
+            color: green;
+        }
+
+        .carrier-contacts {
+            display: flex;
+            align-items: center;
+        }
     }
-    .carrier-contacts {
-      display: flex;
-      align-items: center;
-    }
-  }
 }
 </style>
