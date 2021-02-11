8f533198048472928dfd971628aa838b05934f5d
zia <zia.csco@gmail.com>
2021-02-11 10:04:07 +0430

    wor

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,7 +40,11 @@ return [
                 'useTLS' => false,
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'http',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ],
             ],
         ],
 
